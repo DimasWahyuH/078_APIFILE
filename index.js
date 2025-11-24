9792e85cc800020af75ceea8b5aa11a283f47b5c
dimas <dimaswh19@gmail.com>
2025-11-24 11:03:10 +0700

    membuat index.js

--- index.js
+++ index.js
@@ -9,12 +9,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/v1', apiRoute);
-
 async function startServer() {
     await connectDatabase();
     app.listen(PORT, () => {
         console.log(`Server is running on http://localhost:${PORT}`);
     });
 }
-
 startServer();
